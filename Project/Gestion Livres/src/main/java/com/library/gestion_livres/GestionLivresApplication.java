65dc75c434a6374a082414426a2a9eeea04f4109
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-02 18:14:03 +0100

    update project new version structure

--- Project/Gestion Livres/src/main/java/com/library/gestion_livres/GestionLivresApplication.java
+++ Project/Gestion Livres/src/main/java/com/library/gestion_livres/GestionLivresApplication.java
@@ -1,4 +1,4 @@
-package com.livre.gestion.livres;
+package com.library.gestion_livres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestionLivresApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionLivresApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestionLivresApplication.class, args);
+    }
 
 }
--- Project/Gestion Livres/src/test/java/com/library/gestion_livres/GestionLivresApplicationTests.java
+++ Project/Gestion Livres/src/test/java/com/library/gestion_livres/GestionLivresApplicationTests.java
@@ -1,4 +1,4 @@
-package com.livre.gestion.livres;
+package com.library.gestion_livres;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GestionLivresApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
